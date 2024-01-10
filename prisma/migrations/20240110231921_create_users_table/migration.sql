abfd3663cb315b0a1a2504f61dfefd2f03fdcfaa
tharsila <tharsilapaula@gmail.com>
2024-01-10 20:46:08 -0300

    feat: tabela users

---
+++ prisma/migrations/20240110231921_create_users_table/migration.sql
@@ -0,0 +1,11 @@
+-- CreateTable
+CREATE TABLE `users` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(191) NULL,
+    `email` VARCHAR(191) NOT NULL,
+    `password` VARCHAR(191) NULL,
+    `updatedAt` DATETIME(3) NULL,
+    `deletedAt` DATETIME(3) NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
