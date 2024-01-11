d6b6f1bfcb445e25bb442c3e1025e8dfa140882e
tharsila <tharsilapaula@gmail.com>
2024-01-11 20:10:00 -0300

    feat: pet entity

--- src/modules/pets/entities/pet.entity.ts
+++ src/modules/pets/entities/pet.entity.ts
@@ -1,1 +1,12 @@
-export class Pet {}
+export class Pet {
+  id?: number;
+  name: string;
+  about: string;
+  image: string;
+  location: string;
+  age: number;
+  weight: number;
+  race: string;
+  size: string;
+  since: string;
+}
