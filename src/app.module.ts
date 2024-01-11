f244382a18aeb6cbdd95aaecdc14c17ae58f1743
tharsila <tharsilapaula@gmail.com>
2024-01-11 19:59:25 -0300

    feat: pets e adoptions module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './modules/users/users.module';
+import { PetsModule } from './modules/pets/pets.module';
+import { AdoptionsModule } from './modules/adoptions/adoptions.module';
 
 @Module({
-  imports: [UsersModule],
+  imports: [UsersModule, PetsModule, AdoptionsModule],
   controllers: [AppController],
   providers: [AppService],
 })
