4030b150423a3e9d2ee29a677dabbb474942198f
tharsila <tharsilapaula@gmail.com>
2024-01-11 21:18:18 -0300

    feat: preenche CreatePetDto

--- src/modules/pets/dto/create-pet.dto.ts
+++ src/modules/pets/dto/create-pet.dto.ts
@@ -1,1 +1,40 @@
-export class CreatePetDto {}
+import { IsNotEmpty, IsNumber, IsString } from 'class-validator';
+import { Pet } from '../entities/pet.entity';
+
+export class CreatePetDto extends Pet {
+  @IsNotEmpty()
+  @IsString()
+  name: string;
+
+  @IsNotEmpty()
+  @IsString()
+  about: string;
+
+  @IsNotEmpty()
+  @IsString()
+  image: string;
+
+  @IsNotEmpty()
+  @IsString()
+  location: string;
+
+  @IsNotEmpty()
+  @IsNumber()
+  age: number;
+
+  @IsNotEmpty()
+  @IsNumber()
+  weight: number;
+
+  @IsNotEmpty()
+  @IsString()
+  race: string;
+
+  @IsNotEmpty()
+  @IsString()
+  size: string;
+
+  @IsNotEmpty()
+  @IsString()
+  since: string;
+}
