58fcb4e08d4d54426e10ba6e66735852bceccedf
tharsila <tharsilapaula@gmail.com>
2024-01-11 21:17:59 -0300

    feat: importa prismaModule e injeta PetsRepository no modulo de Pets

--- src/modules/pets/pets.module.ts
+++ src/modules/pets/pets.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { PetsService } from './pets.service';
 import { PetsController } from './pets.controller';
+import { PetsRepository } from './pets.repository';
+import { PrismaModule } from 'src/database/prisma.module';
 
 @Module({
+  imports: [PrismaModule],
   controllers: [PetsController],
-  providers: [PetsService],
+  providers: [PetsService, PetsRepository],
 })
 export class PetsModule {}
