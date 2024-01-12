addfbee1bbe1c14f0a87ded15200b059477d34d3
tharsila <tharsilapaula@gmail.com>
2024-01-11 21:15:12 -0300

    feat: opções ao validationPipe

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,18 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalPipes(
+    new ValidationPipe({
+      // Converte automaticamente dados para os tipos esperados nas classes DTO.
+      transform: true,
+
+      // Permite apenas propriedades validadas, ignorando dados não esperados.
+      whitelist: true,
+
+      // Lança um erro se a solicitação contiver dados não validados.
+      forbidNonWhitelisted: true,
+    }),
+  );
   await app.listen(3000);
 }
 bootstrap();
