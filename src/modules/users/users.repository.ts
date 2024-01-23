6c03015f0c2ad5718804dcb9cce4e58b92b6fac8
tharsila <tharsilapaula@gmail.com>
2024-01-23 20:37:37 -0300

    feat: UsersRepository e metodos de CRUD

---
+++ src/modules/users/users.repository.ts
@@ -0,0 +1,46 @@
+import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/database/prisma.service';
+import { User } from './entities/user.entity';
+
+@Injectable()
+export class UsersRepository {
+  constructor(private prisma: PrismaService) {}
+
+  async createUser(user: User): Promise<User> {
+    return await this.prisma.user.create({
+      data: user,
+    });
+  }
+
+  async findAllUsers(): Promise<User[]> {
+    return await this.prisma.user.findMany();
+  }
+
+  async findOneUser(id: number): Promise<User> {
+    return await this.prisma.user.findFirst({
+      where: {
+        id,
+      },
+    });
+  }
+
+  async updateUser(id: number, user: User): Promise<User> {
+    return await this.prisma.user.update({
+      where: {
+        id,
+      },
+      data: user,
+    });
+  }
+
+  async removeUser(id: number): Promise<void> {
+    await this.prisma.user.update({
+      where: {
+        id,
+      },
+      data: {
+        deletedAt: new Date(),
+      },
+    });
+  }
+}
