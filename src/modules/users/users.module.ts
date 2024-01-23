4703c8b31cec2bad6aac0d5a326286921ce8616a
tharsila <tharsilapaula@gmail.com>
2024-01-23 20:37:10 -0300

    feat: importa PrismaModule e prover UsersRepository

--- src/modules/users/users.module.ts
+++ src/modules/users/users.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
+import { UsersRepository } from './users.repository';
+import { PrismaModule } from 'src/database/prisma.module';
 
 @Module({
+  imports: [PrismaModule],
   controllers: [UsersController],
-  providers: [UsersService],
+  providers: [UsersService, UsersRepository],
 })
 export class UsersModule {}
