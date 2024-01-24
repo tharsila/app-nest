30d27fa6967182a7bff2dcb60cf7d2af3e3c1d6a
tharsila <tharsilapaula@gmail.com>
2024-01-23 21:20:17 -0300

    chore: adiciona bcrypt

--- src/modules/users/users.service.ts
+++ src/modules/users/users.service.ts
@@ -1,15 +1,18 @@
 import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { UsersRepository } from './users.repository';
 
 @Injectable()
 export class UsersService {
+  constructor(private readonly usersRepository: UsersRepository) {}
   create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+    //return 'This action adds a new user';
+    return this.usersRepository.createUser(createUserDto);
   }
 
   findAll() {
-    return `This action returns all users`;
+    return this.usersRepository.findAllUsers();
   }
 
   findOne(id: number) {
