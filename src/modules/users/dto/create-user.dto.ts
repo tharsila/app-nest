6153494dc5717df2bcaafa63bb0200e086637587
tharsila <tharsilapaula@gmail.com>
2024-01-23 21:27:18 -0300

    feat: CreateUserDto

--- src/modules/users/dto/create-user.dto.ts
+++ src/modules/users/dto/create-user.dto.ts
@@ -1,1 +1,12 @@
-export class CreateUserDto {}
+import { User } from '../entities/user.entity';
+import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
+export class CreateUserDto extends User {
+  @IsString({ message: 'O nome deve ter caracteres alfanuméricos' })
+  name: string;
+
+  @IsEmail({}, { message: 'Por favor, insira um endereço de e-mail válido' })
+  email: string;
+
+  @IsNotEmpty({ message: 'Preenchimento obrigatório' })
+  password: string;
+}
