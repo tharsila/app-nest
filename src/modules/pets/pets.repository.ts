f1cdf316a065d9f71b55e9a10376261990ead8a5
tharsila <tharsilapaula@gmail.com>
2024-01-29 22:04:20 -0300

    feat: adiciona metodos crud para pets

--- src/modules/pets/pets.repository.ts
+++ src/modules/pets/pets.repository.ts
@@ -6,11 +6,43 @@ import { Pet } from './entities/pet.entity';
 export class PetsRepository {
   constructor(private prisma: PrismaService) {}
 
-  async createPet(pet: Pet): Promise<Pet> {
+  async createdPet(pet: Pet): Promise<Pet> {
     const createdPet = await this.prisma.pet.create({
       data: pet,
     });
 
     return createdPet;
+  }
+
+  async findAllPets(): Promise<Pet[]> {
+    return await this.prisma.pet.findMany();
+  }
+
+  async findPetById(id: number): Promise<Pet> {
+    return await this.prisma.pet.findFirst({
+      where: {
+        id,
+      },
+    });
+  }
+
+  async updatedPetById(id: number, pet: Pet): Promise<Pet> {
+    return await this.prisma.pet.update({
+      where: {
+        id,
+      },
+      data: pet,
+    });
+  }
+
+  async removePetById(id: number): Promise<void> {
+    await this.prisma.pet.update({
+      where: {
+        id,
+      },
+      data: {
+        deletedAt: new Date(),
+      },
+    });
   }
 }
