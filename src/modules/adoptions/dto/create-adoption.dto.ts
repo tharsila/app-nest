7f00e15d1fa733e923e1654a1e9d893c6c50a614
tharsila <tharsilapaula@gmail.com>
2024-05-09 22:45:33 -0300

    feat: adiciona propriedades ao dto

--- src/modules/adoptions/dto/create-adoption.dto.ts
+++ src/modules/adoptions/dto/create-adoption.dto.ts
@@ -1,1 +1,18 @@
-export class CreateAdoptionDto {}
+import { IsNotEmpty, IsNumber, Max, Min } from 'class-validator';
+import { Adoption } from '../entities/adoption.entity';
+
+export class CreateAdoptionDto extends Adoption {
+  @IsNotEmpty()
+  @IsNumber()
+  @Min(10, { message: 'O preço mínimo de apadrinhamento é R$10,00' })
+  @Max(100, { message: 'O preço máximo de apadrinhamento é R$100,00' })
+  price: number;
+
+  @IsNotEmpty()
+  @IsNumber()
+  pet_id: number;
+
+  @IsNotEmpty()
+  @IsNumber()
+  user_id: number;
+}
--- src/modules/adoptions/dto/update-adoption.dto.ts
+++ src/modules/adoptions/dto/update-adoption.dto.ts
@@ -1,4 +1,3 @@
-import { PartialType } from '@nestjs/mapped-types';
 import { CreateAdoptionDto } from './create-adoption.dto';
 
-export class UpdateAdoptionDto extends PartialType(CreateAdoptionDto) {}
+export class UpdateAdoptionDto extends CreateAdoptionDto {}
