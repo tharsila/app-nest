b3e873aaa986a447b7ba984218d77c89534158f5
tharsila <tharsilapaula@gmail.com>
2024-05-09 21:31:18 -0300

    feat: importa PrismaModule e prover AdoptionsRepository, UsersRepository e PetsRepository em AdoptionsModule

--- src/modules/adoptions/adoptions.module.ts
+++ src/modules/adoptions/adoptions.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
 import { AdoptionsService } from './adoptions.service';
 import { AdoptionsController } from './adoptions.controller';
+import { AdoptionsRepository } from './adoptions.repository';
+import { PrismaModule } from 'src/database/prisma.module';
+import { UsersRepository } from '../users/users.repository';
+import { PetsRepository } from '../pets/pets.repository';
 
 @Module({
+  imports: [PrismaModule],
   controllers: [AdoptionsController],
-  providers: [AdoptionsService],
+  providers: [
+    AdoptionsService,
+    AdoptionsRepository,
+    UsersRepository,
+    PetsRepository,
+  ],
 })
 export class AdoptionsModule {}
